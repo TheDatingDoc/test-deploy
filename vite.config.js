8da160f97c5a317ae3ff69da3ea2dd66f289b517
elenagurchinskaia <136741210+elenagurchinskaia@users.noreply.github.com>
2025-02-06 22:35:16 -0600

    test

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  proxy: {
+    "/graphql": {
+      // target: "http://localhost:3000", //DEV
+      target: "https://the-dating-doc-backend-77b41ab88289.herokuapp.com/", //PROD
+      changeOrigin: true,
+      secure: false,
+    },
+  },
 })
